ac1f743950e698a477bf6e45dbfa1e90d815ae7a
Benedek <fausztb@gmail.com>
2017-12-13 15:10:02 +0100

    Axis arrows

--- cos.py
+++ cos.py
@@ -7,6 +7,44 @@ WIDTH = 700; HEIGHT = 500
 BLACK = (0, 0, 0);          WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0);     RED = (255, 0, 0)
 
+#Functions
+def getNormalized(vector):
+    summa = vector[0] + vector[1]
+    vector[0] /= summa
+    vector[1] /= summa
+    return vector
+
+def getReversed(vector):
+    return [-x for x in vector]
+
+def getProduct(vector, number):
+    return [number * x for x in vector]
+
+def getRotatedUnitVector(vector, degree):
+    actualDeg = math.degrees(math.asin(vector[0])) 
+    actualDeg += degree
+    return [math.sin(math.radians(actualDeg)), 
+            math.cos(math.radians(actualDeg))
+           ]
+
+def drwAxis(surface, start, end, thickness):
+    drw = pygame.draw.line
+    drw(surface, WHITE, start, end, thickness)
+
+    vector = [x-y for x, y in zip(end, start)]
+    vector = getNormalized(vector)
+    vector = getReversed(vector)
+    left =  getRotatedUnitVector(vector, -30)
+    right = getRotatedUnitVector(vector, 30) 
+    left = getProduct(left, 10)
+    right= getProduct(right,10)
+
+    left = [x + y for x, y in zip(left , end)]
+    right= [x + y for x, y in zip(right, end)]
+    print(vector)
+    drw(surface, WHITE, end, left, thickness)
+    drw(surface, WHITE, end, right, thickness)
+
 
 class Circle:
 
@@ -31,16 +69,13 @@ class Circle:
     def _drwMovingCircle(self):
         pygame.draw.circle(self.surface, YELLOW, self.edgepos, self.thick)
     def _drwCoordinateSys(self):
-        drw = pygame.draw.line
-        start = list(self.pos);           end = list(self.pos) 
-        start[1] += int(self.radius * 1.2)
-        end[1] -= int(self.radius *1.2)
+        start = list(self.pos);             end = list(self.pos) 
+        start[1] += int(self.radius * 1.2); end[1] -= int(self.radius *1.2)
+        drwAxis(self.surface, start, end, self.pathThickness)
 
-        drw(self.surface, WHITE, start, end, self.pathThickness)
-        start = list(self.pos);           end = list(self.pos) 
-        start[0] += int(self.radius * 1.2)
-        end[0] -= int(self.radius *1.2)
-        drw(self.surface, WHITE, start, end, self.pathThickness)
+        start = list(self.pos);             end = list(self.pos) 
+        start[0] -= int(self.radius * 1.2); end[0] += int(self.radius *1.2)
+        drwAxis(self.surface, start, end, self.pathThickness)
 
     @property
     def rad(self):
